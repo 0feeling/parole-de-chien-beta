cfbb9d477b58b8126c2f72546f3dfd9afb57c7d6
0feeling <timothee.meunier@gmail.com>
2025-02-07 02:52:38 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
     <section className="w-full h-[600px] bg-gradient-to-br from-[rgb(0,20,137)] to-[rgb(0,181,226)] border-b-[9px] border-b-[rgb(212,93,0)]">
       <div className="grid grid-cols-1 items-center px-16 h-full text-white md:grid-cols-2">
-        <div className="flex flex-col gap-2 md:pr-2">
+        <div className="flex flex-col gap-2 md:pr-4">
           {/* Ajustement avec translate-y responsive */}
           <h1 className="text-7xl font-bold transform md:translate-x-6 md:-translate-y-9 sm:translate-y-0 font-museo">
             Parole de chien
